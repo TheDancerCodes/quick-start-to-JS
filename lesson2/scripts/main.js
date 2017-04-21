fba7808e257e5aa75618b97cd7245a2e53c1025d
Roger Taracha <rojtaracha@gmail.com>
2017-04-21 19:38:57 +0300

    Lesson 2:   + Introduction to operators.

---
+++ lesson2/scripts/main.js
@@ -0,0 +1,46 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+2 + 2;
+/*
+4
+*/
+2 - 2;
+/*
+0
+*/
+2783253863279832903 + 23213123123213293490283490231;
+/*
+2.321312312599655e+28
+*/
+
+2 * 2;
+/*
+4
+*/
+
+2 / 2;
+/*
+1
+*/
+
+10 % 3;
+/*
+1
+*/
+
+30 - 2 * 3;
+/*
+24
+*/
+
+(30 - 2) * 3;
+/*
+84
+*/
